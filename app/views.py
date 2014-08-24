7d11c1c151a07c4ac1a85117d2b58cf00a00c9a3
Lynn Tran <lynn.tran18@gmail.com>
2014-08-24 13:08:42 -0400

    Removed stupid ove mistake with token

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app
 from app import searchrequest
 from flask import render_template, redirect
 from flask import request
+import json
 
 @app.route('/')
 @app.route('/index')
@@ -15,10 +16,16 @@ def search():
     requests = searchrequest.search(term, location)
     name = []
     link = []
+    rating = []
     is_open = []
+    deal = []
     length = requests.values()[2]
     for item in length:
         name.append(item['name']),
         link.append(item['url']),
         is_open.append(item['is_closed'])
-    return render_template('search.html', term = term, location = location, name = name, link = link, is_open = is_open, length = length)
+        rating.append(item['rating'])
+    print "------------------------------"
+    print json.dumps(requests.values()[2], indent=4)
+    return render_template('search.html', term = term, location = location, name = name, link =
+            link, rating = rating, length = length, is_open = is_open)
